a3ccb43ab5915980d242657d59f72818ba82894d
Carl Griffin <carl.griffin@yahoo.com>
2021-07-19 15:38:48 -0500

    latest changes

--- acquire.py
+++ acquire.py
@@ -1,5 +1,6 @@
 def acquire(file_name, column_names):
     '''
-    This function reads in a csv file and certain column names within that file.
+    This function is designed to read a csv using a seperator and build a dataframe 
+    from a specific list of columns specified by the user.
     '''
     return pd.read_csv(file_name, sep="\s", header=None, names=column_names, usecols=[0, 2, 3, 4, 5])
